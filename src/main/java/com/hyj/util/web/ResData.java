dad11be7d86949ca5c666277bb4d4884b5c0451a
huyuanjia <554501252@qq.com>
2019-04-05 16:14:58 +0800

    web修改

--- src/main/java/com/hyj/util/web/ResData.java
+++ src/main/java/com/hyj/util/web/ResData.java
@@ -1,38 +1,38 @@
-package com.hyj.util.web;
-
-import lombok.Data;
-
-@Data
-public class ResData {
-    private boolean status= false;
-    private int code=0;
-    private String msg;
-    private Object data;
-
-    public ResData(boolean status, int code, String msg, Object data) {
-
-        this.status = status;
-        this.code = code;
-        this.msg = msg;
-        this.data = data;
-    }
-
-    /**
-     * 正常数据返回
-     * @param data data
-     */
-    public ResData(Object data) {
-        this.data = data;
-        this.status = true;
-    }
-
-    /**
-     * 错误数据返回
-     * @param code code
-     * @param msg msg
-     */
-    public ResData(int code, String msg) {
-        this.code = code;
-        this.msg = msg;
-    }
-}
+//package com.hyj.util.web;
+//
+//import lombok.Data;
+//
+//@Data
+//public class ResData {
+//    private boolean status= false;
+//    private int code=0;
+//    private String msg;
+//    private Object data;
+//
+//    public ResData(boolean status, int code, String msg, Object data) {
+//
+//        this.status = status;
+//        this.code = code;
+//        this.msg = msg;
+//        this.data = data;
+//    }
+//
+//    /**
+//     * 正常数据返回
+//     * @param data data
+//     */
+//    public ResData(Object data) {
+//        this.data = data;
+//        this.status = true;
+//    }
+//
+//    /**
+//     * 错误数据返回
+//     * @param code code
+//     * @param msg msg
+//     */
+//    public ResData(int code, String msg) {
+//        this.code = code;
+//        this.msg = msg;
+//    }
+//}
